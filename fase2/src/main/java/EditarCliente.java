727512723752aa9a67ecc9b897d0e67e6436d28b
Ricardo Silvério <rvsilverio.2001@gmail.com>
2022-06-06 09:39:03 +0100

    EditarCliente_mostrarTabela_clienteSelecionado

--- fase2/src/main/java/EditarCliente.java
+++ fase2/src/main/java/EditarCliente.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
+import java.util.LinkedList;
 
 public class EditarCliente extends JFrame {
     private JPanel painelEditarCliente;
     private JButton btnGuardar;
     private JButton btnCancelar;
-    private JComboBox comboBox1;
-    private JTable table1;
+    private JComboBox cbEditar;
+    private JTable listaClienteEditar;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
@@ -16,18 +18,30 @@ public class EditarCliente extends JFrame {
     private JTextField textField7;
     private JTextField textField8;
 
+    private DadosAplicacao dadosAplicacao;
+    private Cliente cliente;
+
     public EditarCliente(String title, DadosAplicacao dadosAplicacao) {
         super(title);
-
-
-        btnCancelar.addActionListener(this::btnCancelarButtonActionPerformed);
-
-
         setContentPane(painelEditarCliente);
         pack();
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
         setLocationRelativeTo(null);
+
+        LinkedList<Cliente> clientes = dadosAplicacao.INSTANCIA.getClientes();
+
+
+        for (Cliente cliente : clientes) {
+            String[] clientes_nomes = {cliente.getNome()};
+            String aux = clientes_nomes[0];
+            cbEditar.addItem(aux);
+        }
+
+        btnCancelar.addActionListener(this::btnCancelarButtonActionPerformed);
+        cbEditar.addActionListener(this::cbEditarActionPerformed);
+
+
     }
 
     private void btnCancelarButtonActionPerformed(ActionEvent e) {
@@ -35,5 +49,36 @@ public class EditarCliente extends JFrame {
         toBack();
     }
 
+    private void cbEditarActionPerformed(ActionEvent e){
+        pack();
+        setVisible(true);
+
+        LinkedList<Cliente> clientes = dadosAplicacao.INSTANCIA.getClientes();
+        Object cliente_selecionado = cbEditar.getSelectedItem();
+        for (Cliente cliente : clientes) {
+            if((cliente.getNome()).equals(cliente_selecionado)) {
+                criarTabela(cliente);
+                //break;
+            }
+        }
+
+    }
+
+
+    private void criarTabela(Cliente cliente) {
+
+        String[] cabecalhos = {"Nome", "Morada", "Género", "Data de Nascimento", "Contacto", "Email", "NIF", "Estado Profissional"};
+
+        DefaultTableModel modelo = new DefaultTableModel(cabecalhos,0);
+
+
+        Object[] objects = {cliente.getNome(), cliente.getMorada(), cliente.getGenero(), cliente.getDataNascimento(), cliente.getContacto(), cliente.getEmail(), cliente.getnif(), cliente.getEstadoProfissional()};
+        modelo.addRow(objects);
+
+
+        listaClienteEditar.setModel(modelo);
+        listaClienteEditar.setEnabled(false);
+        listaClienteEditar.getTableHeader().setReorderingAllowed(false);
+    }
 
 }
