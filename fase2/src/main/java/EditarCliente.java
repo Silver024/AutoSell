c404a47ae186f4cfd2c172fcc0fed95eebd8ff3e
Ricardo Silvério <rvsilverio.2001@gmail.com>
2022-06-06 16:24:48 +0100

    EditarCliente Finalizado

--- fase2/src/main/java/EditarCliente.java
+++ fase2/src/main/java/EditarCliente.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 import java.util.LinkedList;
 
 public class EditarCliente extends JFrame {
@@ -9,14 +13,14 @@ public class EditarCliente extends JFrame {
     private JButton btnCancelar;
     private JComboBox cbEditar;
     private JTable listaClienteEditar;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTextField textField7;
-    private JTextField textField8;
+    private JTextField textFieldNif;
+    private JTextField textFieldContacto;
+    private JTextField textFieldEstadoProfissional;
+    private JTextField textFieldEmail;
+    private JTextField textFieldDataNascimento;
+    private JTextField textFieldGenero;
+    private JTextField textFieldMorada;
+    private JTextField textFieldNome;
 
     private DadosAplicacao dadosAplicacao;
     private Cliente cliente;
@@ -38,6 +42,7 @@ public class EditarCliente extends JFrame {
             cbEditar.addItem(aux);
         }
 
+        btnGuardar.addActionListener(this::btnGuardarButtonActionPerformed);
         btnCancelar.addActionListener(this::btnCancelarButtonActionPerformed);
         cbEditar.addActionListener(this::cbEditarActionPerformed);
 
@@ -62,6 +67,65 @@ public class EditarCliente extends JFrame {
             }
         }
 
+    }
+
+    private void btnGuardarButtonActionPerformed(ActionEvent e){
+        LinkedList<Cliente> clientes = dadosAplicacao.INSTANCIA.getClientes();
+        String cliente_selecionado = (String) cbEditar.getSelectedItem();
+        for (Cliente cliente : clientes) {
+            if((cliente.getNome()).equals(cliente_selecionado)) {
+               if(!textFieldNome.getText().isEmpty()){
+                   cliente.setNome(textFieldNome.getText());
+               }
+               if(!textFieldMorada.getText().isEmpty()){
+                cliente.setMorada(textFieldMorada.getText());
+               }
+                if(!textFieldGenero.getText().isEmpty()){
+                    cliente.setGenero(textFieldGenero.getText());
+                }
+                if(!textFieldDataNascimento.getText().isEmpty()){
+                    String dateFormat = "dd/MM/uuuu";
+                    DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+                            .ofPattern(dateFormat)
+                            .withResolverStyle(ResolverStyle.STRICT);
+
+
+                    try {
+                        LocalDate date = LocalDate.parse(textFieldDataNascimento.getText(), dateTimeFormatter);
+                    } catch (DateTimeParseException pe) {
+                        JOptionPane.showMessageDialog(null,"Data de nascimento: '"+ textFieldDataNascimento.getText() +"' inválido", "Erro", JOptionPane.INFORMATION_MESSAGE);
+                        return;
+                    }
+                    cliente.setDataNascimento(textFieldDataNascimento.getText());
+                }
+                if(!textFieldContacto.getText().isEmpty()){
+                    try {
+                        Double.parseDouble(textFieldContacto.getText());
+                    } catch(NumberFormatException pe){
+                        JOptionPane.showMessageDialog(null,"Contacto: '"+ textFieldContacto.getText() +"' inválido", "Erro", JOptionPane.INFORMATION_MESSAGE);
+                        return ;
+                    }
+                 cliente.setContacto(textFieldContacto.getText());
+                }
+                if(!textFieldNif.getText().isEmpty()){
+                    try {
+                        Double.parseDouble(textFieldNif.getText());
+                    } catch(NumberFormatException pe){
+                        JOptionPane.showMessageDialog(null,"NIF: '"+ textFieldNif.getText() +"' inválido", "Erro", JOptionPane.INFORMATION_MESSAGE);
+                        return ;
+                    }
+                 cliente.setNif(textFieldNif.getText());
+                }
+                if(!textFieldEstadoProfissional.getText().isEmpty()){
+                 cliente.setEstadoProfissional(textFieldEstadoProfissional.getText());
+                }
+                if(!textFieldEmail.getText().isEmpty()){
+                 cliente.setEmail(textFieldEmail.getText());
+                }
+            }
+        }
+        setVisible(false);
+        toBack();
     }
 
 
